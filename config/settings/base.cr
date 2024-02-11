ca660d63d633103cb031d66041895e58c4459007
mnsh5 <hugo.orlando2050@gmail.com>
2024-02-11 20:45:29 -0300

    initial commit

---
+++ config/settings/base.cr
@@ -0,0 +1,33 @@
+Marten.configure do |config|
+  # IMPORTANT: please ensure that the secret key value is kept secret!
+  config.secret_key = "__insecure_a8182c2fe3a2f07d27237d36fda5bed6e83e728fd64733587e5ef318e7b1bb0e__"
+
+  # Installed applications
+  # https://martenframework.com/docs/development/reference/settings#installed_apps
+  config.installed_apps = [] of Marten::Apps::Config.class
+
+  # Application middlewares
+  # https://martenframework.com/docs/development/reference/settings#middleware
+  config.middleware = [
+    Marten::Middleware::Session,
+    Marten::Middleware::Flash,
+    Marten::Middleware::GZip,
+    Marten::Middleware::XFrameOptions,
+  ]
+
+  # Databases
+  # https://martenframework.com/docs/development/reference/settings#database-settings
+  config.database do |db|
+    db.backend = :sqlite
+    db.name = Path["todo-app.db"].expand
+  end
+
+  # Templates context producers
+  # https://martenframework.com/docs/development/reference/settings#context_producers
+  config.templates.context_producers = [
+    Marten::Template::ContextProducer::Request,
+    Marten::Template::ContextProducer::Flash,
+    Marten::Template::ContextProducer::Debug,
+    Marten::Template::ContextProducer::I18n,
+  ]
+end
